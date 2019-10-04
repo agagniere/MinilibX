b2b8d6afdee0052d4a3016fbf303ebfe8cd063ab
Antoine <sid.xxdzs@gmail.com>
2019-10-04 20:48:31 +0200

    Handle the closing of a window For now closing a single window will exit the mlx_loop

--- X11/mlx_loop.c
+++ X11/mlx_loop.c
@@ -1,38 +1,52 @@
 /*
-** mlx_loop.c for MiniLibX in 
-** 
+** mlx_loop.c for MiniLibX in
+**
 ** Made by Charlie Root
 ** Login   <ol@epitech.net>
-** 
+**
 ** Started on  Wed Aug  2 18:58:11 2000 Charlie Root
 ** Last update Fri Sep 30 14:47:41 2005 Olivier Crouzet
 */
 
-
-#include	"mlx_int.h"
+#include "mlx_int.h"
+#include <stdbool.h>
 
 extern int	(*(mlx_int_param_event[]))();
 
-
 int		mlx_loop(t_xvar *xvar)
 {
-  XEvent	ev;
-  t_win_list	*win;
+	XEvent      ev;
+	t_win_list* win;
+	Atom        wmDeleteMessage = XInternAtom(xvar->display, "WM_DELETE_WINDOW", False);
+	bool        keep_looping = true;
 
-  mlx_int_set_win_event_mask(xvar);
-  xvar->do_flush = 0;
-  while (42)
-    {
-      while (!xvar->loop_hook || XPending(xvar->display))
+	win = xvar->win_list;
+	while (win)
 	{
-	  XNextEvent(xvar->display,&ev);
-	  win = xvar->win_list;
-	  while (win && (win->window!=ev.xany.window))
-	    win = win->next;
-	  if (win && ev.type < MLX_MAX_EVENT)
-	    if (win->hooks[ev.type].hook)
-	      mlx_int_param_event[ev.type](xvar, &ev, win);
+		XSetWMProtocols(xvar->display, win->window, &wmDeleteMessage, 1);
+		win = win->next;
 	}
-      xvar->loop_hook(xvar->loop_param);
-    }
+	mlx_int_set_win_event_mask(xvar);
+	xvar->do_flush = 0;
+	while (keep_looping)
+	{
+		while (!xvar->loop_hook || XPending(xvar->display))
+		{
+			XNextEvent(xvar->display,&ev);
+			if (ev.type == ClientMessage && ev.xclient.data.l[0] == wmDeleteMessage)
+			{
+				keep_looping = false;
+				break ;
+			}
+			win = xvar->win_list;
+			while (win && (win->window != ev.xany.window))
+				win = win->next;
+			if (win && ev.type < MLX_MAX_EVENT)
+				if (win->hooks[ev.type].hook)
+					mlx_int_param_event[ev.type](xvar, &ev, win);
+		}
+		xvar->loop_hook(xvar->loop_param);
+	}
+	XCloseDisplay(xvar->display);
+	return (0);
 }
