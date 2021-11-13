e89f8d28efae1d52355faf77759607dfcd1837b5
Antoine GAGNIERE <antoine.gagniere@yahoo.com>
2021-11-13 01:32:37 +0100

    Finalize packaging with conan

--- conanfile.py
+++ conanfile.py
@@ -1,35 +1,69 @@
-from conans import ConanFile
-from conans import tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanInvalidConfiguration
+import os
 
 class MinilibXConan(ConanFile):
     name = "minilibx"
     version = "2.0"
-    license = "MIT"
+    license = "BSD"
     author = "agagniere sid.xxdzs@gmail.com"
     url = "https://github.com/agagniere/MinilibX"
     description = "Simple X11 interface"
     topics = ("conan", "x11")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": True, "fPIC": True}
+    settings = ("os", "compiler", "arch")
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "optimisation": ['0', '1', '2', '3', 's', 'fast'],
+        "debug": [True, False]
+    }
+    default_options = {
+        "shared": True,
+        "fPIC": False,
+        "optimisation": '2',
+        "debug": True
+    }
     generators = "make"
-    requires = "xorg/system"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def validate(self):
+        if self.settings.os not in ["Macos", "Linux"]:
+            raise ConanInvalidConfiguration(f"OS {self.settings.os} not supported")
+
+    def requirements(self):
+        if self.settings.os == "Linux":
+            self.requires("xorg/system")
+        else: # MacOS
+            self.requires("opengl/system")
+
+    @property
+    def _source_subfolder(self):
+        if self.settings.os == "Linux":
+            return "X11"
+        elif tools.os_info.os_version >= "11":
+            return "sierra"
+        else:
+            return "elcapitan"
 
     def source(self):
         self.run("git clone " + self.url + " .")
 
+    def configure(self):
+        del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+
     def build(self):
-        self.run("bash ./configure")
-        self.run("make")
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.flags = [f"-O{self.options.optimisation}"]
+        if self.options.debug:
+            autotools.flags += ["-g"]
+        build_env = autotools.vars
+        build_env["MLX_FOLDER"] = self._source_subfolder
+        autotools.make(args=["shared" if self.options.shared else "static", "-j"],vars=build_env)
 
     def package(self):
-        self.copy("mlx.h", dst="include")
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy(os.path.join(self._source_subfolder, "mlx.h"), dst="include", keep_path=False)
+        self.copy("libmlx.so", dst="lib")
+        self.copy("libmlx.a", dst="lib")
 
     def package_info(self):
-        self.cpp_info.libs = ["minilibx"]
+        self.cpp_info.libs = ["mlx", "Xext", "Xpm", "X11"]
